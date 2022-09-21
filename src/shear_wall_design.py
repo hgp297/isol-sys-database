18c7b9d5b37a5b64528dce60ecbaa6e226c7f92f
unknown <hgp@berkeley.edu>
2022-09-20 17:40:01 -0700

    min reinf ratios shear wall design

--- src/shear_wall_design.py
+++ src/shear_wall_design.py
@@ -14,6 +14,18 @@
 
 from design_superstructure import get_story_forces
 
+def get_bar_area(bar_num):
+    return {
+        3 : 0.11,
+        4 : 0.2,
+        5 : 0.31, 
+        6 : 0.44,
+        7 : 0.6,
+        8 : 0.79,
+        9 : 1.0,
+        10 : 1.27
+    }.get(bar_num)
+
 
 ft = 12.0
 ksi = 1000.0
@@ -54,6 +66,27 @@ f_c = 4000.0*psi
 f_yc = 60000.0*psi
 f_ys = 75000.0*psi
 
+# bar spacing
+# design spec: try changing s_t as a function of b_w or l_w
+# spacings and ratios: ACI 318-19 11.6.2, 11.7.3.1, 2.2
+
+# transverse
+bar_t = 4
+b_w = 10.0
+s_t = 16.0
+rho_t = max((2*get_bar_area(bar_t))/(s_t*b_w), 0.0025)
+print('Transv reinf:', rho_t)
+
+# longitudinal
+bar_l = 5
+s_l = 18.0
+rho_l = max(2*get_bar_area(bar_l)/(s_l*b_w), 0.0025)
+rho_l_min = max(0.0025+0.5*(2.5-b_w/l_w)*(rho_t - 0.0025), 0.0025)
+
+rho_l = max(rho_l, rho_l_min)
+print('Longi reinf:', rho_l)
+
+
 # required area
 # all assuming normalweight concrete
 A_cv = V_u / (4*(f_c**(0.5)))
@@ -61,10 +94,14 @@ b_w = ceil(A_cv / l_w)
 
 # P-M design
 # loads already account combos and LL reduction
+
 M_u = np.sum(Fx*hx)
 M_n = M_u / 0.65 # phi for Grade 60 steel
 P_u = np.sum(wLoad)*(90*ft)
-rho_l = 0.0025
+beta_1 = 0.85 # ACI 318-19, T 22.2.2.4.3
+
+#rho_l = 0.0025
+
 T_s2= rho_l*f_ys*(0.6*l_w)*b_w
 T_s1 = (M_n - P_u*(0.4*l_w) - T_s2*(0.4*l_w))/(0.8*l_w)
 A_s1 = T_s1/f_ys
