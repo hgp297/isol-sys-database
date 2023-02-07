d24497ffb2377635591958c3e98429ab6ae9a596
hgp297 <hgp@berkeley.edu>
2023-02-06 18:29:15 -0800

    tfp design complete

--- src/design_bearing.py
+++ src/design_bearing.py
@@ -130,8 +130,8 @@ def design_LRB(T_m, zeta_m, W_tot, r_init, S_1, t_r, N_rb, N_Pb):
     
     return(d_Pb, d_r)
 
-def design_TFP(T_m, zeta_M, S_1, Q, T_2, rho_k):
-    
+# perform one iteration of TFP design to return a damping coefficient
+def iterate_TFP(zeta_guess, mu_1, S_1, T_m, Q, rho_k):
     from numpy import interp
     g  = 386.4
     pi = 3.14159
@@ -141,35 +141,94 @@ def design_TFP(T_m, zeta_M, S_1, Q, T_2, rho_k):
     BmRef   = [0.8, 1.0, 1.2, 1.5, 1.7, 1.9, 2.0]
     
     # from T_m, zeta_M, S_1
-    B_m = interp(zeta_M, zetaRef, BmRef)
+    B_m = interp(zeta_guess, zetaRef, BmRef)
     D_m = g*S_1*T_m/(4*pi**2*B_m)
     
     k_M = (2*pi/T_m)**2 * (1/g)
     
-    W_m = zeta_M*(2*pi*k_M*D_m**2)
+    u_y = 0.01
     
+    k_0 = mu_1/u_y
+    
+    # from Q and D_m
+    k_2 = (k_M*D_m - Q)/D_m
+    R_2 = 1/(2*k_2)
+    
+    # from rho_k
+    u_a = Q/(k_2*(rho_k-1))
+    k_a = rho_k*k_2
+    mu_2 = u_a*k_a
+    R_1 = u_a/(2*(mu_2-mu_1))
+    
+    # effective design values
+    a = 1/(2*R_1)
+    b = 1/(2*R_2)
+    k_e = (mu_2 + b*(D_m - u_a))/D_m
+    W_e = 4*(mu_2 - b*u_a)*D_m - 4*(a-b)*u_a**2 - 4*(k_0 -a)*u_y**2
+    zeta_E   = W_e/(2*pi*k_e*D_m**2)
+    
+    err = (zeta_E - zeta_guess)**2
+    
+    return(err)
+    
+def design_TFP(T_m, S_1, Q, rho_k):
     
     # guess
     import random
     # random.seed(985)
-    mu_Q_coef = random.uniform(0.3, 0.8)
+    mu_Q_coef = random.uniform(0.3, 0.6)
     mu_1 = mu_Q_coef*Q
     
-    # R_1 = random.uniform(30.0, 60.0)
-    u_y = 0.01
+    # converge design on damping
+    # design will achieve T_m, Q, rho_k as specified
+    from scipy.optimize import minimize_scalar
+    res = minimize_scalar(iterate_TFP, args=(mu_1, S_1, T_m, Q, rho_k),
+                             bounds=(0.01, 0.35), method='bounded')
+
+    zeta_m = res.x
     
-    # # from Q
-    # mu_1 = Q + u_y/(2*R_1)
+    # finish design on converged damping
+    from numpy import interp
+    g  = 386.4
+    pi = 3.14159
+    
+    # from ASCE Ch. 17, get damping multiplier
+    zetaRef = [0.02, 0.05, 0.10, 0.20, 0.30, 0.40, 0.50]
+    BmRef   = [0.8, 1.0, 1.2, 1.5, 1.7, 1.9, 2.0]
+    
+    # from T_m, zeta_M, S_1
+    B_m = interp(zeta_m, zetaRef, BmRef)
+    D_m = g*S_1*T_m/(4*pi**2*B_m)
+    
+    k_M = (2*pi/T_m)**2 * (1/g)
+    
+    # W_m = zeta_M*(2*pi*k_M*D_m**2)
+    
+    u_y = 0.01
     
     k_0 = mu_1/u_y
     
-    # from T_2
-    k_2 = (2*pi/T_2)**2 * (1/g)
+    # from Q and D_m
+    k_2 = (k_M*D_m - Q)/D_m
     R_2 = 1/(2*k_2)
     
-    u_a = (4*Q*D_m - W_m)/(4*(Q - mu_1))
-    mu_2 = Q + u_a/(2*R_2)
-    R_1 = u_a/(2*(mu_2 - mu_1))
+    # from rho_k
+    u_a = Q/(k_2*(rho_k-1))
+    k_a = rho_k*k_2
+    mu_2 = u_a*k_a
+    R_1 = u_a/(2*(mu_2-mu_1))
+    
+    # effective design values
+    a = 1/(2*R_1)
+    b = 1/(2*R_2)
+    k_e = (mu_2 + b*(D_m - u_a))/D_m
+    W_e = 4*(mu_2 - b*u_a)*D_m - 4*(a-b)*u_a**2 - 4*(k_0 -a)*u_y**2
+    zeta_E   = W_e/(2*pi*k_e*D_m**2)
+    T_e = 2*pi*(1/(g*k_e))**0.5
+    
+    # u_a = (4*Q*D_m - W_m)/(4*(Q - mu_1))
+    # mu_2 = Q + u_a/(2*R_2)
+    # R_1 = u_a/(2*(mu_2 - mu_1))
     
     # u_a = D_m - W_m/(4*Q)
     # mu_2 = Q + u_a/(2*R_2)
@@ -190,30 +249,21 @@ def design_TFP(T_m, zeta_M, S_1, Q, T_2, rho_k):
     # u_a = 2*mu_1*R_1/(2*k_a*R_1 - 1)
     # mu_2 = u_a * k_a
     
-    # need to figure out how to ensure design reaches W_m
-    a = 1/(2*R_1)
-    b = 1/(2*R_2)
-    k_e = (mu_2 + b*(D_m - u_a))/D_m
-    W_e = 4*(mu_2 - b*u_a)*D_m - 4*(a-b)*u_a**2 - 4*(k_0 -a)*u_y**2
-    zeta_E   = W_e/(2*pi*k_e*D_m**2)
-    T_e      = 2*pi*(1/(g*k_e))**0.5
-    print(T_e)
+    print('Effective period: ', T_e)
+    print('Effective damping: ', zeta_E)
     
     mu_list = [mu_1, mu_2, mu_2]
     R_list = [R_1, R_2, R_2]
     
     return(mu_list, R_list)
     
-    
-    
 if __name__ == '__main__':
-    T_m = 3.5
-    T_2 = 4.5
-    zeta_M = 0.1
+    T_m = 4.5
+    zeta_M = 0.084
     S_1 = 1.017
-    Q = 0.05
-    rho_k = 75.0
-    mus, Rs = design_TFP(T_m, zeta_M, S_1, Q, T_2, rho_k)
+    Q = 0.03
+    rho_k = 7.0
+    mus, Rs = design_TFP(T_m, S_1, Q, rho_k)
     print(mus)
     print(Rs)
     # design_LRB()
