e77bc6222369c0128305f49cf517417858c3728e
hgp297 <hgp@berkeley.edu>
2023-03-01 14:27:50 -0800

    added workbook for braced frame modeling

--- src/db.py
+++ src/db.py
@@ -21,6 +21,7 @@ main_obj.design_bearings(filter_designs=True)
 main_obj.design_structure()
 
 test_mf = main_obj.mf_designs.iloc[0]
+test_cbf = main_obj.tfp_designs.iloc[3]
 
 # test build one building (MF, TFP only)
 from building import Building
