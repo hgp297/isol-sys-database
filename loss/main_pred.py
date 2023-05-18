8d241b492c3707af5426c778a85b2ad5e288e2b4
unknown <hgp@berkeley.edu>
2023-05-18 10:16:06 -0700

    some updates to plotting

--- loss/main_pred.py
+++ loss/main_pred.py
@@ -1154,6 +1154,126 @@ print('Predicted collapse risk: ',
 print('Predicted peak interstory drift: ',
       f'{design_PID:.2%}')
 
+#%% cost sensitivity
+
+land_costs = [2151., 3227., 4303., 5379.]
+steel_costs = [1., 2., 3., 4.]
+
+import numpy as np
+gap_price_grid = np.zeros([4,4])
+Ry_price_grid = np.zeros([4,4])
+Tm_price_grid = np.zeros([4,4])
+zetaM_price_grid = np.zeros([4,4])
+
+percent_of_replacement = 1.0
+cost_thresh = percent_of_replacement*8.1e6
+ok_cost = X_space.loc[space_repair_cost[cost_var+'_pred']<=cost_thresh]
+
+# <2 weeks for a team of 50
+dt_thresh = 1e6
+ok_time = X_space.loc[space_downtime[time_var+'_pred']<=dt_thresh]
+
+risk_thresh = 0.025
+ok_risk = X_space.loc[space_collapse_risk['collapse_risk_pred']<=
+                      risk_thresh]
+
+# risk_thresh = 0.025
+# ok_risk = X_space.loc[space_drift['max_drift_pred']<=
+#                       risk_thresh]
+
+X_design = X_space[np.logical_and.reduce((
+        X_space.index.isin(ok_cost.index), 
+        X_space.index.isin(ok_time.index),
+        X_space.index.isin(ok_risk.index)))]
+
+for idx_l, land in enumerate(land_costs):
+    for idx_s, steel in enumerate(steel_costs):
+        steel_price = steel
+        coef_dict = get_steel_coefs(df, steel_per_unit=steel_price)
+        
+        lcps = land/(3.28**2)
+        upfront_costs = calc_upfront_cost(X_design, coef_dict, 
+                                          land_cost_per_sqft=lcps)
+        
+        cheapest_design_idx = upfront_costs.idxmin()
+        design_upfront_cost = upfront_costs.min()
+
+        # least upfront cost of the viable designs
+        best_design = X_design.loc[cheapest_design_idx]
+        gap_price_grid[idx_l][idx_s] = best_design['gapRatio']
+        Ry_price_grid[idx_l][idx_s] = best_design['RI']
+        Tm_price_grid[idx_l][idx_s] = best_design['Tm']
+        zetaM_price_grid[idx_l][idx_s] = best_design['zetaM']
+        
+#%%
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+axis_font = 18
+subt_font = 18
+label_size = 16
+import matplotlib as mpl
+mpl.rcParams['xtick.labelsize'] = label_size 
+mpl.rcParams['ytick.labelsize'] = label_size 
+
+steel_rows = ['$1.00', '$2.00', '$3.00', '$4.00']
+land_cols=['$200', '$300', '$400', '$500', ]
+# print(gap_price_grid)
+# print(Ry_price_grid)
+# print(Tm_price_grid)
+# print(zetaM_price_grid)
+
+gap_df = pd.DataFrame(data=gap_price_grid,
+                      index=land_cols,
+                      columns=steel_rows)
+
+Ry_df = pd.DataFrame(data=Ry_price_grid,
+                      index=land_cols,
+                      columns=steel_rows)
+
+Tm_df = pd.DataFrame(data=Tm_price_grid,
+                      index=land_cols,
+                      columns=steel_rows)
+
+zetaM_df = pd.DataFrame(data=zetaM_price_grid,
+                      index=land_cols,
+                      columns=steel_rows)
+
+# Draw a heatmap with the numeric values in each cell
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.close('all')
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4.5))
+sns.heatmap(gap_df, annot=True, fmt='.3g', cmap='Blues', cbar=False,
+            linewidths=.5, ax=ax1)
+ax1.set_xlabel('Steel cost per lb.', fontsize=axis_font)
+ax1.set_ylabel('Land cost per sq ft.', fontsize=axis_font)
+ax1.set_title('Gap ratio', fontsize=subt_font)
+
+sns.heatmap(Ry_df, annot=True, fmt='.3g', cmap='Blues', cbar=False,
+            linewidths=.5, ax=ax2, yticklabels=False)
+ax2.set_xlabel('Steel cost per lb.', fontsize=axis_font)
+ax2.set_title(r'$R_y$', fontsize=subt_font)
+
+sns.heatmap(Tm_df, annot=True, fmt='.3g', cmap='Blues', cbar=False,
+            linewidths=.5, ax=ax3, yticklabels=False)
+ax3.set_xlabel('Steel cost per lb.', fontsize=axis_font)
+ax3.set_title(r'$T_M$', fontsize=subt_font)
+fig.tight_layout()
+
+#%%
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
+# Load the example flights dataset and convert to long-form
+flights_long = sns.load_dataset("flights")
+flights = flights_long.pivot("month", "year", "passengers")
+
+# Draw a heatmap with the numeric values in each cell
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(flights, annot=True, fmt="d", linewidths=.5, ax=ax)
 #%% Fit costs (SVR, across all data)
 
 ## fit impacted set
