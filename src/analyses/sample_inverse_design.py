069d000d524cf3c5b41c3ce55564723d26a8da6e
unknown <hgp@berkeley.edu>
2024-07-24 18:57:04 -0700

    sample inverse

---
+++ src/analyses/sample_inverse_design.py
@@ -0,0 +1,214 @@
+############################################################################
+#               Sample inverse design
+
+############################################################################
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '../')
+
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from doe import GP
+
+plt.rcParams["text.usetex"] = True
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+title_font=20
+axis_font = 18
+subt_font = 18
+label_size = 16
+mpl.rcParams['xtick.labelsize'] = label_size 
+mpl.rcParams['ytick.labelsize'] = label_size 
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+warnings.filterwarnings("ignore", category=FutureWarning) 
+pd.options.mode.chained_assignment = None  
+
+plt.close('all')
+
+main_obj = pd.read_pickle("../../data/loss/structural_db_parallel_loss.pickle")
+    
+main_obj.calculate_collapse()
+
+df_raw = main_obj.ops_analysis
+df_raw = df_raw.reset_index(drop=True)
+
+# remove the singular outlier point
+from scipy import stats
+df = df_raw[np.abs(stats.zscore(df_raw['collapse_prob'])) < 5].copy()
+
+# df = df.drop(columns=['index'])
+# df = df_whole.head(100).copy()
+
+df['max_drift'] = df.PID.apply(max)
+df['log_drift'] = np.log(df['max_drift'])
+
+df['max_velo'] = df.PFV.apply(max)
+df['max_accel'] = df.PFA.apply(max)
+
+df['T_ratio'] = df['T_m'] / df['T_fb']
+df['T_ratio_e'] = df['T_m'] / df['T_fbe']
+pi = 3.14159
+g = 386.4
+
+zetaRef = [0.02, 0.05, 0.10, 0.20, 0.30, 0.40, 0.50]
+BmRef   = [0.8, 1.0, 1.2, 1.5, 1.7, 1.9, 2.0]
+df['Bm'] = np.interp(df['zeta_e'], zetaRef, BmRef)
+
+df['gap_ratio'] = (df['constructed_moat']*4*pi**2)/ \
+    (g*(df['sa_tm']/df['Bm'])*df['T_m']**2)
+
+df_loss = main_obj.loss_data
+
+max_obj = pd.read_pickle("../../data/loss/structural_db_parallel_max_loss.pickle")
+df_loss_max = max_obj.max_loss
+
+#%%
+# make a generalized 2D plotting grid, defaulted to gap and Ry
+# grid is based on the bounds of input data
+def make_2D_plotting_space(X, res, x_var='gap_ratio', y_var='RI', 
+                           all_vars=['gap_ratio', 'RI', 'T_ratio', 'zeta_e'],
+                           third_var_set = None, fourth_var_set = None,
+                           x_bounds=None, y_bounds=None):
+    
+    if x_bounds == None:
+        x_min = min(X[x_var])
+        x_max = max(X[x_var])
+    else:
+        x_min = x_bounds[0]
+        x_max = x_bounds[1]
+    if y_bounds == None:
+        y_min = min(X[y_var])
+        y_max = max(X[y_var])
+    else:
+        y_min = y_bounds[0]
+        y_max = y_bounds[1]
+    xx, yy = np.meshgrid(np.linspace(x_min,
+                                     x_max,
+                                     res),
+                         np.linspace(y_min,
+                                     y_max,
+                                     res))
+
+    rem_vars = [i for i in all_vars if i not in [x_var, y_var]]
+    third_var = rem_vars[0]
+    fourth_var = rem_vars[-1]
+       
+    xx = xx
+    yy = yy
+    
+    if third_var_set is None:
+        third_var_val= X[third_var].median()
+    else:
+        third_var_val = third_var_set
+    if fourth_var_set is None:
+        fourth_var_val = X[fourth_var].median()
+    else:
+        fourth_var_val = fourth_var_set
+    
+    
+    X_pl = pd.DataFrame({x_var:xx.ravel(),
+                         y_var:yy.ravel(),
+                         third_var:np.repeat(third_var_val,
+                                             res*res),
+                         fourth_var:np.repeat(fourth_var_val, 
+                                              res*res)})
+    X_plot = X_pl[all_vars]
+                         
+    return(X_plot)
+
+def make_design_space(res, var_list=['gap_ratio', 'RI', 'T_ratio', 'zeta_e'],
+                      fixed_var=None):
+    
+    bound_dict = {
+        'gap_ratio': (0.6, 1.5),
+        'RI': (0.5, 2.25),
+        'T_ratio': (2.0, 11.0),
+        'zeta_e': (0.1, 0.25),
+        'k_ratio': (5.0, 12.0)}
+    
+    fixed_val = {
+        'gap_ratio': 1.0,
+        'RI': 2.0,
+        'T_ratio': 4.0,
+        'zeta_e': 0.15,
+        'k_ratio': 10.0
+        }
+    
+    if fixed_var is None:
+        xx, yy, uu, vv = np.meshgrid(np.linspace(*bound_dict[var_list[0]], res),
+                                     np.linspace(*bound_dict[var_list[1]], res),
+                                     np.linspace(*bound_dict[var_list[2]], res),
+                                     np.linspace(*bound_dict[var_list[3]], res))
+        
+        X_space = pd.DataFrame({var_list[0]:xx.ravel(),
+                             var_list[1]:yy.ravel(),
+                             var_list[2]:uu.ravel(),
+                             var_list[3]:vv.ravel()})
+    else:
+        fixed_val_single = fixed_val[fixed_var]
+        excluded_idx = var_list.index(fixed_var)
+        my_args = tuple(np.linspace(*bound_dict[var_list[i]], res) 
+                                    for i in range(len(var_list)) 
+                                    if i!=excluded_idx)
+        
+        xx, yy, uu, vv = np.meshgrid(*my_args,
+                                     fixed_val_single)
+        
+        var_list.pop(excluded_idx)
+        
+        # TODO: this is unordered
+        X_space = pd.DataFrame({var_list[0]:xx.ravel(),
+                             var_list[1]:yy.ravel(),
+                             var_list[2]:uu.ravel(),
+                             fixed_var:vv.ravel()})
+    return(X_space)
+#%% normalize DVs and prepare all variables
+df['bldg_area'] = df['L_bldg']**2 * (df['num_stories'] + 1)
+
+df['replacement_cost'] = 600.0*(df['bldg_area'])
+df['total_cmp_cost'] = df_loss_max['cost_50%']
+df['cmp_replace_cost_ratio'] = df['total_cmp_cost']/df['replacement_cost']
+df['median_cost_ratio'] = df_loss['cost_50%']/df['replacement_cost']
+df['cmp_cost_ratio'] = df_loss['cost_50%']/df['total_cmp_cost']
+
+# but working in parallel (2x faster)
+df['replacement_time'] = df['bldg_area']/1000*365
+df['total_cmp_time'] = df_loss_max['time_l_50%']
+df['cmp_replace_time_ratio'] = df['total_cmp_time']/df['replacement_time']
+df['median_time_ratio'] = df_loss['time_l_50%']/df['replacement_time']
+df['cmp_time_ratio'] = df_loss['time_l_50%']/df['total_cmp_time']
+
+df['replacement_freq'] = df_loss['replacement_freq']
+
+df[['B_50%', 'C_50%', 'D_50%', 'E_50%']] = df_loss[['B_50%', 'C_50%', 'D_50%', 'E_50%']]
+
+df['impacted'] = pd.to_numeric(df['impacted'])
+
+cost_var = 'cmp_cost_ratio'
+time_var = 'cmp_time_ratio'
+covariate_list = ['gap_ratio', 'RI', 'T_ratio', 'zeta_e']
+
+mask = df['B_50%'].isnull()
+
+df['B_50%'].loc[mask] = df_loss_max['B_50%'].loc[mask]
+df['C_50%'].loc[mask] = df_loss_max['C_50%'].loc[mask]
+df['D_50%'].loc[mask] = df_loss_max['D_50%'].loc[mask]
+df['E_50%'].loc[mask] = df_loss_max['E_50%'].loc[mask]
+
+#%% subsets
+
+df_no_impact = df[df['impacted'] == 0]
+
+# remove the singular outlier point
+from scipy import stats
+df_no_impact = df_no_impact[np.abs(stats.zscore(df_no_impact['cmp_cost_ratio'])) < 5].copy()
+
+df_tfp = df_no_impact[df_no_impact['isolator_system'] == 'TFP']
+df_lrb = df_no_impact[df_no_impact['isolator_system'] == 'LRB']
+df_cbf = df_no_impact[df_no_impact['superstructure_system'] == 'CBF']
+df_mf = df_no_impact[df_no_impact['superstructure_system'] == 'MF']
