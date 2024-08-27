b9d6bb2b3d369a0be7995b90810e463bcd2dd226
unknown <hgp@berkeley.edu>
2024-08-26 21:11:48 -0700

    again no wipe data in scratch

--- src/val_ida_thread.py
+++ src/val_ida_thread.py
@@ -49,7 +49,7 @@ def ida_run_row(row_num, run_case_str):
     data_path = '../data/validation/'+run_case_str+'/'
     
     if os.path.exists(data_path):
-        shutil.rmtree(data_path)
+        pass
     else:
         os.makedirs(data_path)
     
